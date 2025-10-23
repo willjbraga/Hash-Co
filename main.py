cabaddba7a4c39844da68d162e97bf9a2a9c7682
willjbraga <155175134+willjbraga@users.noreply.github.com>
2025-10-23 17:21:03 -0300

    Criado um ambiente virtual venv, arquivo de requirements para versões de cada biblioteca usada no projeto e o sistema de login usando streamlit-authenticator

---
+++ main.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+import streamlit_authenticator as stauth
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+# Parte de Autenticação usando o streamlit_authenticator 
+
+senhas_criptografadas = stauth.Hasher(["123", "456", "789"]).generate()
+
+credenciais = {"usernames": {
+       "lira@gmail.com": {"name": "Lira", "password": senhas_criptografadas[0]},
+       "alon@gmail.com": {"name": "Alon", "password": senhas_criptografadas[1]},
+       "amanda@gmail.com": {"name": "Amanda", "password": senhas_criptografadas[2]}
+}}
+
+#Estrutura: credenciais, cookie name, cookie key, cookie expire days
+authenticator = stauth.Authenticate(credenciais, "credenciais_hashco", "123456789abcdefg", cookie_expiry_days=14)
+
+#O authenticator_status pode ter 3 valores: True(caso esteja tudo certo), False(Caso as credenciais esteja errado) e None(Caso não tenha preenchido)
+def autenticar_usuario(authenticator):
+    nome, authenticator_status, username = authenticator.login()
+
+    if authenticator_status:
+        return {"nome": nome, "username": username}
+    elif authenticator_status == False:
+        st.error("Combinação de usuário e senha inválidas")
+    else:
+        st.error("Preencha o formulário para fazer login")
+
+
+def logout(authenticator):
+    authenticator.logout()
+
+dados_usuario = autenticar_usuario(authenticator)
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+
+if dados_usuario:
+    @st.cache_data
+    def carregar_dados():
+        tabela = pd.read_excel("Base.xlsx")
+        return tabela
+
+    base = carregar_dados()
+
+    st.title("Hash&Co")
+    st.write("Bem vindo, Fulano")
+    st.table(base.head(10))
+
