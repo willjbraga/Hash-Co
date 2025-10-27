5efc100caf5a1e74483499d764d70e8c12e4f765
willjbraga <williamjubraga@gmail.com>
2025-10-27 11:17:54 -0300

    Alterações na página Home Page. Foi criado botões e suas funcionalidades de navegação, containers e colunas para organização da pagina e a adição de imagens

--- homepage.py
+++ homepage.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
-st.title("HomePage")
+
+coluna_esquerda, coluna_direita =  st.columns([1, 1.5])
+
+# tudo que era st."alguma coisa" vira coluna_esquerda pois estára na minha coluna esquerda
+
+coluna_esquerda.title("Hash&Co")
+coluna_esquerda.write("#### Bem vindo, Fulano")
+
+botao_dashboard = coluna_esquerda.button("Dashboards Projetos")
+botao_indicadores = coluna_esquerda.button("Principais indicadores")
+#-----------------------------------------------------------------------------------------------
+
+
+
+if botao_dashboard:
+    st.switch_page("dashboard.py")
+
+if botao_indicadores:
+    st.switch_page("indicadores.py")
+
+container = coluna_direita.container(border=True)
+container.image("imagens/time-comunidade.webp")
